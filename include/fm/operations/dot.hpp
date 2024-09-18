08be5a57bad65c4bcde28811c913386e2e61e419
Sam Mish <samuelpmish@gmail.com>
2024-09-18 08:15:17 -0700

    add dot(vec, matrix) and dot(matrix, vec)

--- include/fm/operations/dot.hpp
+++ include/fm/operations/dot.hpp
@@ -2,6 +2,60 @@
 
 namespace fm {
 
+template < Kind kind, u32 m, u32 n, typename S, typename T>
+constexpr auto dot(const vec<m, S> & x, const matrix<kind, m, n, T> & A) {
+
+  if constexpr (kind == Kind::Isotropic || kind == Kind::Diagonal) {
+    static_assert(m == n);
+    return x * A.data;
+  }
+
+  if constexpr (kind == Kind::Skew) {
+    return cross(x, A.data); // ?
+  }
+
+  if constexpr (kind == Kind::Symmetric || 
+                kind == Kind::Rotation ||
+                kind == Kind::Skew ||
+                kind == Kind::General) {
+    vec<n, decltype(S{} + T{})> output{};
+    for (u32 j = 0; j < n; j++) {
+      for (u32 i = 0; i < m; i++) {
+        output(j) += x(i) * A(i,j);
+      }
+    }
+    return output;
+  }
+
+}
+
+template < Kind kind, u32 m, u32 n, typename S, typename T>
+constexpr auto dot(const matrix<kind, m, n, S> & A, const vec<n, T> & x) {
+
+  if constexpr (kind == Kind::Isotropic || kind == Kind::Diagonal) {
+    static_assert(m == n);
+    return A.data * x;
+  }
+
+  if constexpr (kind == Kind::Skew) {
+    return cross(A.data, x); // ?
+  }
+
+  if constexpr (kind == Kind::Symmetric || 
+                kind == Kind::Rotation ||
+                kind == Kind::Skew ||
+                kind == Kind::General) {
+    vec<m, decltype(S{} + T{})> output{};
+    for (u32 i = 0; i < m; i++) {
+      for (u32 j = 0; j < n; j++) {
+        output(i) += A(i,j) * x(j);
+      }
+    }
+    return output;
+  }
+
+}
+
 template < Kind kindA, Kind kindB, u32 m, u32 n, u32 p, typename TA, typename TB>
 constexpr auto dot(const matrix<kindA, m, n, TA> & A, 
                    const matrix<kindB, n, p, TB> & B) {
